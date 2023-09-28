5f16210b00d4085b1e83a80c8634aa89fa61c561
frankalvarez013 <frankalvarez013@gmail.com>
2023-09-28 12:56:59 -0700

    Updated css in Page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -5,11 +5,13 @@ import TopTier from '../public/HighTier.svg'
 
 export default function Home() {
   return (
-    <main className="h-[582px] bg-gradient-blue">
+    <main className="h-[500px] bg-gradient-blue">
       <div className="h-full bg-bottom bg-stack bg-no-repeat bg-fullW">
-        <div className="h-full flex ">
-          <div className="h-full bg-bottom lg:bg-right-bottom bg-[url('/sun.svg')] bg-no-repeat flex-grow flex-shrink-0"></div>
-          <div className="lg:basis-1/3 flex-shrink"></div>
+        <div className="h-full flex">
+          <div className="h-full bg-bottom lg:bg-[bottom_right_35%] bg-[url('/sun.svg')] bg-no-repeat flex flex-grow flex-shrink-0 justify-evenly items-center">
+            <div className="basis-1/2">hi</div>
+            <div className="basis-1/3">hello</div>
+          </div>
         </div>
       </div>
     </main>
