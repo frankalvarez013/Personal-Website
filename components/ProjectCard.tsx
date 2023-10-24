8ae5453113c92400047866f701f5697ad4114c96
Frank Alvarez <frankalvarez013@gmail.com>
2023-10-23 22:33:09 -0700

    Created Card

--- components/ProjectCard.tsx
+++ components/ProjectCard.tsx
@@ -1,15 +1,31 @@
 const ProjectCard = (props) => {
   return (
-    <div className="h-96 p-40 flex justify-end items-start">
-      <div className="font-bold text-base">Location</div>
-      <div>
-        Experience
-        <p>
+    <div className="flex p-5 rounded-xl pb justify-start items-start  flex-wrap">
+      <div className="font-bold mb-3 mr-10">
+        {`${props.startTime} - ${props.endTime}`}
+      </div>
+      <div className=" max-w-sm">
+        <div className="font-semibold text-lg ">
+          {`${props.title} · ${props.business}`}
+        </div>
+        <p className="mb-3 text-sm leading-normal">
           Aliqua id fugiat nostrud irure ex duis ea quis id quis ad et. Sunt qui
           esse pariatur duis deserunt mollit dolore cillum minim tempor enim.
           Elit aute irure tempor cupidatat incididunt sint deserunt ut voluptate
           aute id deserunt nisi.
         </p>
+        <div className="flex gap-2 flex-wrap">
+          {props.Tech.map((value, index) => {
+            return (
+              <div
+                className="rounded-3xl py-[2px] px-3 bg-red-300 bg-opacity-30 text-red-500 text-rose-400"
+                key={index}
+              >
+                {value}
+              </div>
+            )
+          })}
+        </div>
       </div>
     </div>
   )
--- components/ProjectSection.tsx
+++ components/ProjectSection.tsx
@@ -1,15 +1,25 @@
+import ProjectCard from './ProjectCard'
 const ProjectSection = () => {
   return (
     <div
       className="h-[1000px] md:h-[1200px] flex justify-center items-center"
       id="experience"
     >
-      <div className="flex w-5/6 h-5/6 flex-col md:flex-row">
-        <div className="basis-0 md:basis-1/3">
-          <h1 className="font-extrabold text-6xl">Projects & Experience!</h1>
-          <p></p>
+      <div className="flex w-11/12 h-5/6 flex-col md:flex-row justify-start lg:justify-center gap-10">
+        <div className="">
+          <h1 className="font-bold text-2xl whitespace-nowrap">
+            Projects & Experience!
+          </h1>
         </div>
-        <div className="basis-11/12 md:basis-2/3 border-black border-8"></div>
+        <div className=" max-w-[41rem] border-black border-8">
+          <ProjectCard
+            startTime="2018"
+            endTime="present"
+            title="Lead Engineer"
+            business="Upstatement"
+            Tech={['Nextjs', 'React', 'CSS', 'Tailwind', 'Vercel']}
+          ></ProjectCard>
+        </div>
       </div>
     </div>
   )
